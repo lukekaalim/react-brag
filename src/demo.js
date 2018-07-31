097af434a01ac540dc625d5cb3790c218d3a8ae9
Luke Kaalim <luke@kaal.im>
2018-08-01 07:01:44 +1000

    Update demo to take advantage of editors

--- src/demo.js
+++ src/demo.js
@@ -1,16 +1,28 @@
+// @flow
 import React from 'react';
 
 import Brag from './index';
 
-const COMPONENTS = [
-  { name: '<Brag />', factory: () => <p>N/A</p>, },
-];
+const LibraryFactory = ({ library = {} }: any) => <Brag components={[]} library={library} />;
 
 const LIBRARY = {
   name: '🏆 react-brag',
   abstract: 'A small react library to help show off your components',
-  description: '',
+  description: 'React Brag is a react library build to help you quickly show off your components',
+  importStatement: 'import Brag, { Skeleton } from \'react-brag\';',
+  version: '1.0.0',
 };
+
+const COMPONENTS = [
+  {
+    name: '<Brag />',
+    factory: LibraryFactory,
+    propsDesc: [
+      { propName: 'library', propType: { type: 'json' }, initialValue: LIBRARY },
+    ],
+    displayType: 'presentational',
+  },
+];
 
 const BragDemo = () => (
   <Brag
--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@
 import { injectGlobal } from 'styled-components';
 
 injectGlobal`
- @import url('https://fonts.googleapis.com/css?family=Ubuntu+Mono');
+  @import url('https://fonts.googleapis.com/css?family=Ubuntu+Mono');
 `;
 
 import Brag from './components/Brag';
+import Skeleton from './components/Skeleton';
+
+export {
+  Skeleton
+};
 
 export default Brag;
